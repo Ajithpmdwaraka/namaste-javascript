d581f797bccce5d6a62d9d60d985cb95af3a4be0
Ajith PM <ajithpmdwaraka@gmail.com>
2025-09-24 06:10:24 +0530

    hoisting

--- 03-episode3/hoisting.js
+++ 03-episode3/hoisting.js
@@ -1,4 +1,5 @@
 
+getName();
 console.log(x);
 
 var x = 8;
@@ -7,6 +8,4 @@ function getName() {
     console.log("Namaste JavaScript");
 }
 
-getName();
-
-console.log(getName);
+// Hoisting is a phenomen in javascript that allows to access varibles and function even before it is intialized without any error, it is because even before the code is executed memory is allocated variables and function during the memory creation phase
