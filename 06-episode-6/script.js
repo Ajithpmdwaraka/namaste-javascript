0a612c2079bc5979e7e934ffabeaa1b47c5d7aca
Ajith PM <ajithpmdwaraka@gmail.com>
2025-09-25 10:46:16 +0530

    episode-6

---
+++ 06-episode-6/script.js
@@ -0,0 +1,9 @@
+var a;
+
+console.log(a);
+//Show undefined as the variable is declared but not assigned any value
+
+a = 10;
+
+console.log(b);
+//Shows reference error as the varibale is not declared adn not allocated any memory
