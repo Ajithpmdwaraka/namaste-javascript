8c3863195e371b2d257ba02786529d8b074d5af4
Ajith PM <ajithpmdwaraka@gmail.com>
2025-10-08 13:07:21 +0530

    map

---
+++ 19-episode19/map.js
@@ -0,0 +1,11 @@
+//Map() transforms an array into a new array
+arr = [2, 4, 6, 8];
+
+const double = arr.map((i) => i * 2);
+const binary = arr.map((i) => i.toString(2));
+
+console.log(output);
+console.log(binary);
+
+
+
